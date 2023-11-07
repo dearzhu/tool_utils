d0687524f04c27bb55f8d506ac65469aca25956c
zhuxiong <1065650758@qq.com>
2023-11-07 22:34:24 +0800

    添加相关性筛选变量

--- var_method.py
+++ var_method.py
@@ -9,6 +9,8 @@ import pandas as pd
 import numpy as np
 
 from statsmodels.stats.outliers_influence import variance_inflation_factor
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
 def fun_get_cols_vif(x_train):
@@ -42,3 +44,88 @@ def fun_proceed_vif_choose_vars(df, num_cols, excepts_cols, silent=True):
     if not silent:
         print(cols_vif_value)
     return choosed_cols
+
+
+def fun_chose_corr_graph(df, to_num_cols, corr_threshold=0.6, metric_dict={}, only_choose_one=False, slient=False):
+    to_num_cols = list(set(to_num_cols) & set(df.columns))
+    to_num_cols = list(set(to_num_cols) & set(metric_dict.keys()))
+    df_origin = df[to_num_cols].copy()
+    cm_df = df_origin.copy()
+    # 生成网络图时的相关性阈值
+    cm_df = cm_df.reset_index()
+    corr_df = {'c1': [], 'c2': [], 'corr': []}
+    for c1 in list(cm_df['index']):
+        for c2 in list(cm_df.columns):
+            if c2 != 'index' and c1 != c2 and not (c2 in corr_df['c1'] and c1 in corr_df['c2']):
+                corr_df['c1'].append(c1)
+                corr_df['c2'].append(c2)
+                corr_df['corr'].append(cm_df.loc[cm_df['index'] == c1, c2].values[0])
+    corr_df = pd.DataFrame(corr_df)
+    print(corr_df.head(2))
+    corr_df = corr_df[corr_df['corr'] > corr_threshold].copy().reset_index()
+    del corr_df['index']
+    data = corr_df.copy()
+    del corr_df
+    node_list = data['c1'].append(data['c2']).unique()
+    G = nx.Graph()
+    G.add_edges_from(node_list)
+    for i in range(data.shape[0]):
+        if i % 1000 == 0:
+            if not slient:
+                print('---------------------编号：', i, '--------------------')
+        G.add_edge(data['c1'][i], data['c2'][i], weight=data['corr'][i])
+    pos = nx.spring_layout(G)
+    # 输出各个强关联变量是图
+    if not slient:
+        print('------------------生成联通子图------------------------')
+    subgraph_list = [c for c in sorted(nx.connected_components(G), key=len, reverse=True)]
+
+    subgraph_len = [len(c) for c in sorted(nx.connected_components(G), key=len, reverse=True)]
+    # 图中点的信息字典,说明图中点属于哪个图
+    g_node_info = dict()
+    graphs_nodes = []
+    choosed_nodes = []
+    if not slient:
+        print('子图数量：', len(subgraph_list))
+        print('---------------遍历每个子图---------------------')
+    for i in range(len(subgraph_list)):
+        sub_graph = G.subgraph(subgraph_list[i])
+        sub_graph_len = subgraph_len[i]
+        sub_graph_df = data[(data['c1'].isin([sub_id for sub_id in subgraph_list[i]]))].copy()
+        if not slient:
+            print('子图规模：', sub_graph_len)
+        sg = list(sub_graph.nodes)
+        graphs_nodes = graphs_nodes + sg
+        graph_cols = {}
+        for c in sg:
+            if str(c) in metric_dict.keys():
+                graph_cols[c] = metric_dict[c]
+        if only_choose_one:
+            graph_cols = [sorted(zip(graph_cols.values(), graph_cols.keys()), reverse=True)[0]]
+        else:
+            if len(sg) >= 10:
+                graph_cols = sorted(zip(graph_cols.values(), graph_cols.keys()), reverse=True)[0:4]
+            elif len(sg) >= 5:
+                graph_cols = sorted(zip(graph_cols.values(), graph_cols.keys()), reverse=True)[0:3]
+            elif len(sg) >= 3:
+                graph_cols = sorted(zip(graph_cols.values(), graph_cols.keys()), reverse=True)[0:2]
+            else:
+                graph_cols = [sorted(zip(graph_cols.values(), graph_cols.keys()), reverse=True)[0]]
+        graph_col_names = []
+        for v in graph_cols:
+            v0, v1 = v
+            graph_col_names.append(v1)
+        choosed_nodes = choosed_nodes + graph_col_names
+        corr_matrix = pd.pivot_table(data[(data['c1'].isin(sg)) & (data['c2'].isin(sg))], index='c1', columns='c2',
+                                     values='corr', aggfunc='mean')
+        # 如果是不输出信息的模式
+        if not slient:
+            plt.figure(figsize=[10, 6])
+            nx.draw(sub_graph, nx.spring_layout(sub_graph), node_color='red', with_labels=True, edge_color='black',
+                    node_size=50, font_size=16)
+            plt.show()
+    print('全部变量数量：', len(metric_dict.keys()))
+    print('按照强相关性阈值构造强相关性关系网中的变量数量：', len(graphs_nodes))
+    print('选出强相关性关系网中的变量数量：', len(list(choosed_nodes)))
+    print('最终选出的变量数量：', len(list(set(metric_dict.keys()) - set(graphs_nodes)) + list(choosed_nodes)))
+    return list(set(metric_dict.leys()) - set(graphs_nodes)) + list(choosed_nodes)
